3354def8d0cdb31ae166aa630e6837e47e01f2cc
hasan-1 <hasanjasser551@gmail.com>
2025-02-09 20:14:13 +0200

    arithmetic operation

---
+++ src/arithmetic.ts
@@ -0,0 +1,23 @@
+
+
+let num1:number = 12;
+let num2:number = 50 ;
+
+const sum:number = num1 + num2;
+console.log(sum);
+
+const sub:number = num1 - num2;
+console.log(sub);
+
+const multi:number = num1 * num2
+console.log(multi);
+
+const divid:number = num1 / num2;
+console.log(divid);
+
+const remainder:number = num2 % num1 ;
+console.log(remainder);
+
+
+
+
