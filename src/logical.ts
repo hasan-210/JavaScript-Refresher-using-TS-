bb1765f0f2dd452e3a930178f4885867b8dbfc8c
hasan-1 <hasanjasser551@gmail.com>
2025-02-13 14:42:54 +0200

    logical operator

---
+++ src/logical.ts
@@ -0,0 +1,11 @@
+// Logical Operator  ( ||  &&  ! )
+
+
+const x:boolean = false;
+const y:boolean = true;
+const z:boolean = false;
+
+console.log(x && y);      // False
+console.log(x || y);      // True
+console.log(x || y && z); // False
+console.log(!x);          // True
