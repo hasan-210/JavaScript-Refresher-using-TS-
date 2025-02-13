66f86e46d767eb1746ccc1bdf00c91fe5bc4f7b2
hasan-1 <hasanjasser551@gmail.com>
2025-02-13 14:33:29 +0200

    comparison operator

---
+++ src/comparison.ts
@@ -0,0 +1,18 @@
+/** comparison Operator */
+
+// ==  ===   !=  !==
+console.log(5 == "5");   // => True    |   == compare between two parameters by Value
+console.log(5 === "5");  // => False   |   === compare between two parameters by Value and Type
+
+
+console.log(5 != "5");   // => False  |   === compare between two parameters by Value 
+console.log(5 !== "5");  // => True   |   === compare between two parameters by Value and Type
+
+
+// >   <   >=  <=
+const number1 = 30;
+const number2 = 50;
+
+console.log(number1 > number2);  // False
+console.log(number1 < number2);  // True
+
