e0bfe735f2a6bb7a1351f0a578ecb5631842d81e
hasan-1 <hasanjasser551@gmail.com>
2025-02-14 10:16:36 +0200

    This Keyword | 1

---
+++ src/thisKeyword.ts
@@ -0,0 +1,14 @@
+
+
+
+const people = {
+    username:'hasan',
+    age : 25,
+
+    info(){
+        console.log(this);
+        return 'Full information . . .';
+    }
+}
+
+console.log(people.info());
